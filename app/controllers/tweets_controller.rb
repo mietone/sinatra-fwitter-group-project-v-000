013098cc32212b88707dca3e18e1fd17008ddd3e
Miwa <paintfx@gmail.com>
2018-04-01 05:07:07 +0000

    Done.

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -9,4 +9,36 @@ class TweetsController < ApplicationController
     end
   end
 
+  get '/tweets/new' do
+    if logged_in?
+      erb :'/tweets/create_tweet'
+    else
+      redirect "/login"
+    end
+  end
+
+  post '/tweets' do
+    @tweet = Tweet.new(content: params[:content], user: current_user)
+
+    if logged_in?
+      if @tweet.save
+        redirect "/tweets/#{@tweet.id}"
+      else
+        redirect "/tweets/new"
+      end
+    else
+      redirect "/login"
+    end
+
+  end
+
+  get '/tweets/:id' do
+    if logged_in?
+      @tweet = Tweet.find_by(id: params[:id])
+      erb :'/tweets/show'
+    else
+      redirect "/login"
+    end
+  end
+
 end
